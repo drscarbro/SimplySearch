5d2774864334615ea025e27eecd7bfdb93cf3822
Daniel Scarbrough <drscarbro@gmail.com>
2016-04-15 04:32:59 -0500

    Adding JSON Modeler

--- app/src/main/java/com/m2d/simplysearch/MainActivity.java
+++ app/src/main/java/com/m2d/simplysearch/MainActivity.java
@@ -1,18 +1,42 @@
 package com.m2d.simplysearch;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView output;
+    ProgressBar pb;
+    List<queryAsync> tasks;
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,39 +46,136 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         output = (TextView) findViewById(R.id.txtVwResults);
         output.setMovementMethod(new ScrollingMovementMethod());
+        pb = (ProgressBar) findViewById(R.id.progressBar);
+        pb.setVisibility(View.INVISIBLE);
+
+        tasks = new ArrayList<>();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         assert fab != null;
-        fab.setOnClickListener(new View.OnClickListener(){
+        fab.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+
+                if (isOnline()) {
+                    requestData();
+                } else {
+
+                }
+            }
+
+            private void requestData() {
                 queryAsync results = new queryAsync();
-                results.execute("Anything","Something","Another Thing");
+                results.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "Anything", "Something",
+                        "Another Thing");
             }
 
         });
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     protected void updateDisplay(String result) {
         output.append(result + "\n");
     }
 
-    private class queryAsync extends AsyncTask<String, String, String>{
+    protected boolean isOnline(){
+        ConnectivityManager cm = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.m2d.simplysearch/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.m2d.simplysearch/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    private class queryAsync extends AsyncTask<String, String, String> {
 
         @Override
-        protected void onPreExecute(){
+        protected void onPreExecute() {
             updateDisplay("I'm Starting Something!");
+
+            if(tasks.size() == 0){
+                pb.setVisibility(View.VISIBLE);
+            }
+            tasks.add(this);
         }
 
         @Override
         protected String doInBackground(String... params) {
+
+            for (int i = 0; i < params.length; i++) {
+                publishProgress("Working with " + params[i]);
+
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
             return "All done.";
         }
 
         @Override
-        protected void onPostExecute(String results){
+        protected void onPostExecute(String results) {
             updateDisplay(results);
+
+
+            tasks.remove(this);
+            if(tasks.size() == 0){
+                pb.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            updateDisplay(values[0]);
         }
 
     }
---
+++ app/src/main/java/com/m2d/simplysearch/model/Results.java
@@ -0,0 +1,7 @@
+package com.m2d.simplysearch.model;
+
+/**
+ * Created by daniel on 4/15/16.
+ */
+public class Results {
+}
